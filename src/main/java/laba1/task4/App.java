7695b758c4ce9259f00eb0ef269d5651955d6f1b
aelmod <aelmod@mail.ru>
2016-12-07 10:29:12 +0200

    added out files added screenshot for laba6

--- src/main/java/laba1/task4/App.java
+++ src/main/java/laba1/task4/App.java
@@ -3,6 +3,6 @@ package laba1.task4;
 public class App {
     public static void main(String[] args) {
         Triangle triangle = new Triangle(5.15, 5.99);
-        System.out.println(triangle.gipotenuza());
+        System.out.println("Triangle hypotenuse: "+triangle.gipotenuza());
     }
 }
