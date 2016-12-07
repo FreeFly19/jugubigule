770fbe4aba4fa38804cab8e56041a5f0fb0cf6f7
aelmod <aelmod@mail.ru>
2016-12-07 09:03:44 +0200

    added fucking laba3

--- src/main/java/laba3/App.java
+++ src/main/java/laba3/App.java
@@ -5,6 +5,34 @@ package laba3;
  */
 public class App {
     public static void main(String[] args) {
+        Vector3D vector3D1 = new Vector3D(54, 87, 6);
+        Vector3D vector3D2 = new Vector3D(234, 567, 67);
 
+        Vector3D vector3D3 = new Vector3D(54, 87, 6);
+        Vector3D vector3D4 = new Vector3D(234, 567, 67);
+
+        Vector3D vector3D5 = new Vector3D(54, 87, 6);
+
+        Vector3D vector3D6 = new Vector3D(54, 87, 6);
+        Vector3D vector3D7 = new Vector3D(54, 87, 6);
+
+        Vector3D addition = vector3D1.addition(vector3D2);
+        Vector3D subtraction = vector3D4.subtraction(vector3D3);
+        Vector3D multiply = vector3D5.scalarMultiply(10);
+
+        System.out.println("Addition vectors: ");
+        System.out.println(addition.toString());
+        System.out.println("Subtraction vectors: ");
+        System.out.println(subtraction.toString());
+        System.out.println("Multiply vectors: ");
+        System.out.println(multiply.toString());
+        System.out.println("Scalar Product of Vectors: " + vector3D1.scalarProduct(vector3D2));
+        System.out.println("Vertor length: " + vector3D2.vectorLength());
+
+        if (vector3D6.compareVectorLength(vector3D7)) {
+            System.out.println("Vectors identical");
+        } else {
+            System.out.println("Vectors is not identical");
+        }
     }
 }
--- src/main/java/laba3/Vector3D.java
+++ src/main/java/laba3/Vector3D.java
@@ -4,9 +4,9 @@ package laba3;
  * Created by Євгеній on 07.12.2016.
  */
 public class Vector3D {
-    private int x;
-    private int y;
-    private int z;
+    private double x;
+    private double y;
+    private double z;
 
     public Vector3D(int x, int y, int z) {
         this.x = x;
@@ -14,5 +14,48 @@ public class Vector3D {
         this.z = z;
     }
 
+    public Vector3D addition(Vector3D vector) {
+        x = x + vector.x;
+        y = y + vector.y;
+        z = z + vector.z;
+        return Vector3D.this;
+    }
 
+    public Vector3D subtraction(Vector3D vector) {
+        x = x - vector.x;
+        y = y - vector.y;
+        z = z - vector.z;
+        return Vector3D.this;
+    }
+
+    public double scalarProduct(Vector3D vector) {
+        return x * vector.x + y * vector.y + z * vector.z;
+    }
+
+    public Vector3D scalarMultiply(double v) {
+        x = x * v;
+        y = y * v;
+        z = z * v;
+        return Vector3D.this;
+    }
+
+    public double vectorLength() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public boolean compareVectorLength(Vector3D vector){
+        double v = vectorLength();
+        double v1 = vector.vectorLength();
+        if (v==v1){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "X: " + this.x +
+                "\nY: " + this.y +
+                "\nZ: " + this.z;
+    }
 }
